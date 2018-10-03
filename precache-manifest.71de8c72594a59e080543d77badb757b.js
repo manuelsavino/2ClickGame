0a453702b67b4a02b9a3bdcbcec3f6630307002c
manuelsavino <manuelsavino@gmail.com>
2018-10-03 15:47:46 -0400

    Updates

---
+++ precache-manifest.71de8c72594a59e080543d77badb757b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f628351e238fd16a930",
+    "url": "/2ClickGame/static/js/runtime~main.4f628351.js"
+  },
+  {
+    "revision": "8c915f23c6803378134a",
+    "url": "/2ClickGame/static/js/main.8c915f23.chunk.js"
+  },
+  {
+    "revision": "104011e7f39d900ed057",
+    "url": "/2ClickGame/static/js/1.104011e7.chunk.js"
+  },
+  {
+    "revision": "8c915f23c6803378134a",
+    "url": "/2ClickGame/static/css/main.61793a97.chunk.css"
+  },
+  {
+    "revision": "f747b8205fe8638e4a160d1c2751032d",
+    "url": "/2ClickGame/index.html"
+  }
+];
